44d9852fa254b24c3a093db534976baad96d232c
BT_2S14_32 <email@email>
2024-09-17 10:51:25 -0400

    Added all the code for the tip calculator unit 1 project!

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner s = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("| Do not use any symbols when inputting data please! (No $ or %) |");
+        System.out.println("------------------------------------------------------------------");
+
+
+        System.out.print("Enter your bill ($): ");
+        String bill = s.nextLine();
+        System.out.print("Tip Percent: ");
+        String tip = s.nextLine();
+        System.out.print("Number of People: ");
+        String people = s.nextLine();
+        System.out.println();
+        System.out.println();
+
+
+        final Double totalBill = Double.parseDouble(bill);
+        final Integer tipPercent = Integer.parseInt(tip);
+        final Integer numPeople = Integer.parseInt(people);
+
+        double totalTip = ((double)tipPercent/100 * totalBill);
+        double totalWithTip = totalBill + totalTip;
+        double tipPerPerson = totalTip / numPeople;
+        double totalPerPerson = totalWithTip / numPeople;
+
+
+        System.out.println("----------------------------------");
+        System.out.println("Total Tip Amount: $" + df.format(totalTip));
+        System.out.println("----------------------------------");
+
+        System.out.println("Total Bill Cost: $" + df.format(totalWithTip));
+        System.out.println("----------------------------------");
+
+        System.out.println("Tip Per Person: $" + df.format(tipPerPerson));
+        System.out.println("----------------------------------");
+
+        System.out.println("Total Bill Per Person: $" + df.format(totalPerPerson));
+        System.out.println("----------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-}
+    }
